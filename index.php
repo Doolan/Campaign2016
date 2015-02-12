eba2f7b52806df79b435ad197d08a0909619ac10
Doolan <doolansr@rose-hulman.edu>
2015-02-12 08:59:30 -0500

    Uploaded the Print ad

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
 		?>
 <div class="wrapper">
 	<header role="banner">
-
 		<nav>
 			<label for="show-menu" class="show-menu">
 				<img src="images/menu4.gif"></label>
@@ -61,17 +60,17 @@
 				</li>
 				<li>
 					<a href="speeches.html">Speeches ￬</a>
-						<ul class="hidden">
-							<li>
-								<a href="speeches.html#Accident">Breaking News</a>
-							</li>
-							<li>
-								<a href="speeches.html#IssueSpeech">Issue Speech</a>
-							</li>
-							<li>
-								<a href="speeches.html#Announcement">Announcement</a>
-							</li>
-						</ul>
+					<ul class="hidden">
+						<li>
+							<a href="speeches.html#Accident">Breaking News</a>
+						</li>
+						<li>
+							<a href="speeches.html#IssueSpeech">Issue Speech</a>
+						</li>
+						<li>
+							<a href="speeches.html#Announcement">Announcement</a>
+						</li>
+					</ul>
 				</li>
 				<li>
 					<a href="news.html">In the News ￬</a>
@@ -87,74 +86,74 @@
 						</li>
 					</ul>
 				</li>
-				<!--<li>
-				<a href="#">Meet the Team</a>
-			</li>
-			-->
-		</ul>
-	</nav>
-</header>
-<div class="layered">
-	<img src="images/both_27.jpg" class="jumbotron" id="indeximg"/>
-	<div class="layer1"></div>
-	<div class="layer2">
-		<div class="titleText" id="indextext">
-			<h1 id="normaltitleText">Emily Bochnowski For President!</h1>
-			<h2>Steven Doolan For Vice President</h2>
-			<div onclick="joinclick()">
-				<a href="#" id="joinUs" >Join Us!</a>
+				<li>
+					<a href="ad.html">Ad</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<div class="layered">
+		<img src="images/both_27.jpg" class="jumbotron" id="indeximg"/>
+		<div class="layer1"></div>
+		<div class="layer2">
+			<div class="titleText" id="indextext">
+				<h1 id="normaltitleText">Emily Bochnowski For President!</h1>
+				<h2>Steven Doolan For Vice President</h2>
+				<div onclick="joinclick()">
+					<a href="#" id="joinUs" >Join Us!</a>
+				</div>
 			</div>
 		</div>
 	</div>
 	<!--Breaking Newsstyle="display:none;"-->
-	<div  id="breaking" >
+	<div  id="breaking" style="display:none;">
 		<div class="layered">
 			<!--<div id="xbutton" onclick="hideclick1()">
-			<img src="images/x.gif" alt="X"></div>-->
-			<img src="images/accident1.jpg" class="jumbotron"/>
-			<div class="layer1"></div>
-			<div class="layer2">
-				<div class="titleText">
-					<h1>Breaking News</h1>
-					<p><br></p>
-					<h2 id="lily">Lily Escalera hit by a Drunk Driver</h2>
-					<h2 id="changesToTeam">Morgan Escalera steps down as a Presidential Candidate
-					<br>Emily Bochnowski named as replacement
-					</h2>
-				</div>
+			<img src="images/x.gif" alt="X"></div>
+		-->
+		<img src="images/accident1.jpg" class="jumbotron"/>
+		<div class="layer1"></div>
+		<div class="layer2">
+			<div class="titleText">
+				<h1>Breaking News</h1>
+				<p>
+					<br></p>
+				<h2 id="lily">Lily Escalera hit by a Drunk Driver</h2>
+				<h2 id="changesToTeam">
+					Morgan Escalera steps down as a Presidential Candidate
+					<br>Emily Bochnowski named as replacement</h2>
 			</div>
-		</div>
-		<div class="mainText" id="breakingP">
-			<p>
-				"I, Morgan Escalara, have chosen to step down as a presidential candidate. My only daughter was injured by a drunk driver and has hospitalized at Massachusetts General Hospital in the Medical Intensive Care Unit. The doctors have placed her in a medically induced coma..." (<a href="speeches.html#Accident" title="Read More">Continue Reading</a>)
-				<br>
-				-Senator Escalara
-			</p>
-			<p>
-				"I could not image worse circumstances, and it is with great sorrow that I accept the responsibility of replacing Senator Escalera as a Presidential Candidate. I believe my core beliefs overlap extensively with Senator Escalera. I will fight to promote our shared vision of the future.  I hope that the supporters of the Escalera and Doolan campaign will not lose hope in this ticket, but rather will stand behind the new ticket of Bochnowski and Doolan..." (<a href="speeches.html#BochnowskiAnnouncement" title="Read More">Continue Reading</a>)
-				<br>
-					-Senator Bochnowski
-			</p>
-			<p onclick="hideclickAll()" style="text-decoration: underline; cursor:pointer;">
-				Continue to the main page
-			</p>
 		</div>
 	</div>
-
-	<!--JOIN US BOX-->
-	<div style="display:none;" id="register" >
-		<div id="xbutton" onclick="hideclick()">
-			<img src="images/x.gif" alt="X"></div>
-		<h1>Join Us</h1>
-		<form id="emailForm" method="POST">
-			<input type="text" name="email" placeholder="Email Address*">
-			<br>
-			<div id="submitButton" onclick="submitclick()">
-				<a href="#" name="submit">Submit</a>
-			</div>
-		</form>
+	<div class="mainText" id="breakingP">
+		<p>
+			"I, Morgan Escalara, have chosen to step down as a presidential candidate. My only daughter was injured by a drunk driver and has hospitalized at Massachusetts General Hospital in the Medical Intensive Care Unit. The doctors have placed her in a medically induced coma..." (
+			<a href="speeches.html#Accident" title="Read More">Continue Reading</a>
+			)
+			<br>-Senator Escalara</p>
+		<p>
+			"I could not image worse circumstances, and it is with great sorrow that I accept the responsibility of replacing Senator Escalera as a Presidential Candidate. I believe my core beliefs overlap extensively with Senator Escalera. I will fight to promote our shared vision of the future.  I hope that the supporters of the Escalera and Doolan campaign will not lose hope in this ticket, but rather will stand behind the new ticket of Bochnowski and Doolan..." (
+			<a href="speeches.html#BochnowskiAnnouncement" title="Read More">Continue Reading</a>
+			)
+			<br>-Senator Bochnowski</p>
+		<p onclick="hideclickAll()" style="text-decoration: underline; cursor:pointer;">Continue to the main page</p>
 	</div>
 </div>
+
+<!--JOIN US BOX-->
+<div style="display:none;" id="register" >
+	<div id="xbutton" onclick="hideclickAll()">
+		<img src="images/x.gif" alt="X"></div>
+	<h1>Join Us</h1>
+	<form id="emailForm" method="POST">
+		<input type="text" name="email" placeholder="Email Address*">
+		<br>
+		<div id="submitButton" onclick="submitclick()">
+			<a href="#" name="submit">Submit</a>
+		</div>
+	</form>
+</div>
+
 <div style="display:none;" id="blackBackground" onclick="hideclickAll()"></div>
 <!-- <div id="blackBackground"></div>
 -->
@@ -177,6 +176,9 @@
 	</li>
 	<li>
 		<a href="news.html">In the News</a>
+	</li>
+	<li>
+		<a href="ad.html">Ad</a>
 	</li>
 	<!--<li>
 	<a href="#">Meet the Team</a>
--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 
 
-window.onload = function() { 
+
+var  breakingUpdate = function() { 
 	console.log('hit');
 	document.getElementById('blackBackground').style.display = 'block';
+	document.getElementById('breaking').style.display = 'block';
 };
-/**var hideclick1 = function(){
-	document.getElementById('blackBackground').style.display = 'none';
-	document.getElementById('breaking').style.display = 'none';
-	console.log('hit hide');
-};**/
 
 var hideclickAll = function(){
 	document.getElementById('blackBackground').style.display = 'none';
@@ -21,25 +18,45 @@ var joinclick = function(){
 	document.getElementById('blackBackground').style.display = 'block';
 	document.getElementById('register').style.display = 'block';
 };
-/**var hideclick = function(){
-	document.getElementById('blackBackground').style.display = 'none';
-	document.getElementById('register').style.display = 'none';
-};**/
+
 var submitclick = function(){
 	document.getElementById('emailForm').submit();
 	document.getElementById('blackBackground').style.display = 'none';
 	document.getElementById('register').style.display = 'none';
 };
 
-/**document.getElementById('joinUs').click(function(){
-	document.getElementById('blackBackground').show();
-	document.getElementById('register').show();
-});
-document.getElementById('submitButton').click(function(){
-	//send data to php
-	document.getElementById('blackBackground').hide();
-	document.getElementById('register').hide();
-});**/
+var checkcookie = function(){
+	var cookie = getCookie("breakingUpdate");
+	if(cookie !=""){
+		console.log('has cookie',cookie);
+		//loggedin
+	}
+	else{
+		console.log('doesnt have cookie');
+		//log in
+		breakingUpdate();
+		document.cookie="breakingUpdate=TEMP";
+	}
+}
+
+var clearCookies = function(){
+	console.log('killed the cookie');
+		document.cookie = "breakingUpdate=DEAD; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+}
+
+
+//FROM W3
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
 
 // Cache selectors outside callback for performance. 
 /**var $window = $(window);
